a19aeb2c83b6fbc8246962fd2f81e4a9812eeff4
Rahul Virmani <rvirmani@hawk.iit.edu>
2022-06-30 23:37:24 -0500

    cgange2

--- Script.py
+++ Script.py
@@ -1,19 +1,18 @@
 
-    import http
 from http.client import HTTPResponse
 import re
 from unittest import result
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
-urls = ['https://threatpost.com/category/vulnerabilities/', 'https://news.ycombinator.com/']
-links_store = []
+urls = ['https://threatpost.com/category/vulnerabilities', 'https://news.ycombinator.com']
+# links_store = []
 for url in urls:
     request = Request(url)
     web_page = urlopen(request)
     soup = BeautifulSoup(web_page, 'html.parser')
 
-    for link in soup.find_all('a', href=True):
+    for link in soup.find_all('a', attrs={'href': re.compile("^https://")} ):
         links_store = link.get('href')
-        print(links_store + "\n")
-print (len(link))
+        print(link.get('href') + "\n")
+  
