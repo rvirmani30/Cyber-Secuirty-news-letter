88a628ae1f4d79442fc07ccc7867209fcfe5f959
Rahul Virmani <rvirmani@hawk.iit.edu>
2022-07-01 18:55:11 -0500

    commit

--- Script.py
+++ Script.py
@@ -1,18 +1,30 @@
-
 from http.client import HTTPResponse
 import re
 from unittest import result
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+import schedule
+import time
 
-urls = ['https://threatpost.com/category/vulnerabilities', 'https://news.ycombinator.com']
-# links_store = []
-for url in urls:
-    request = Request(url)
-    web_page = urlopen(request)
-    soup = BeautifulSoup(web_page, 'html.parser')
 
-    for link in soup.find_all('a', attrs={'href': re.compile("^https://")} ):
-        links_store = link.get('href')
-        print(link.get('href') + "\n")
-  
+def script():
+    urls = ['https://threatpost.com/category/vulnerabilities', 'https://news.ycombinator.com']
+    # links_store = []
+    for url in urls:
+        request = Request(url)
+        web_page = urlopen(request)
+        soup = BeautifulSoup(web_page, 'html.parser')
+
+        for link in soup.find_all('a', attrs={'href': re.compile("^https://")} ):
+            links_store = link.get('href')
+            print(link.get('href') + "\n")
+
+def scheduling_script():
+    print ("Scedule script ran")
+    schedule.every(1).day.at("18:53").do(script)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__=="__main__":
+    scheduling_script()
